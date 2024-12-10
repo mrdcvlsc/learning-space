5ded22f7551e43ec116aff04d73b706c0c8e735a
Jubal Mordecai Velasco <mrdcvlsc@gmail.com>
2024-12-10 18:57:56 +0800

    Create esp32_send_txt_bt.ino

---
+++ arduino/esp32_send_txt_bt.ino
@@ -0,0 +1,29 @@
+// send text over bluetooth
+
+#include <BluetoothSerial.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial esp32_bt;
+
+void setup() {
+  Serial.begin(115200);
+  esp32_bt.begin("esp32-bluetooth");
+}
+
+void loop() {
+  if (Serial.available()) {
+    Serial.println("Sending Data...");
+
+    String msg = Serial.readString();
+    esp32_bt.println(msg);
+  } else {
+    Serial.println("Standby...");
+  }
+}
